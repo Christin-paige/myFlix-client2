cd9e17f4c24a3f1c1d73412ef35d758f07c934b3
Christin-paige <christinpmartin@gmail.com>
2023-10-23 13:12:59 -0400

    renamed case-sensitive titles

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
-import MyFilms from '../navigation-bar/IndieFilms all of the films worth watching.svg';
+import MyFilms from '../navigation-bar/indiefilms.svg';
 
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
